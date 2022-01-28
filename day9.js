f869320e2bad91deb922605c7d185c447e95e88c
aryan <aryan.me77@yahoo.com>
2022-01-28 20:39:44 +0330

    day9 part2

--- day9.js
+++ day9.js
@@ -8,30 +8,73 @@ const parsingData = (data) => {
   return data.split("\n").map((e) => e.split("").map((e) => Number(e)));
 };
 
-const lowestPoint = (data, i, j) => {
-  const number = data[i][j];
+const adjacents = (heightMap, i, j) => {
   const adjacents = [
-    (data[i - 1] || [])[j],
-    (data[i + 1] || [])[j],
-    data[i][j + 1],
-    data[i][j - 1],
+    (heightMap[i - 1] || [])[j],
+    heightMap[i][j + 1],
+    (heightMap[i + 1] || [])[j],
+    heightMap[i][j - 1],
   ];
-  if (number < Math.min(...adjacents.filter((e) => !(e === undefined)))) {
+  return adjacents;
+};
+
+const isLowPoint = (coordinate, adjacents) => {
+  const adjacentsFilterd = adjacents.filter((e) => !(e === undefined));
+  if (coordinate < Math.min(...adjacentsFilterd)) {
     return true;
   }
   return false;
 };
 
-function main(data) {
-  let arr = [];
-  for (let i = 0; i < data.length; i++) {
-    for (let j = 0; j < data[i].length; j++) {
-      if (lowestPoint(data, i, j)) {
-        arr.push(data[i][j] + 1);
+const addToBasinCoordinate = (basinsCoordinate, i, j) => {
+  basinsCoordinate.push({ i, j });
+};
+
+const isInbasinsCoordinate = (basinsCoordinate, i, j) => {
+  basinsCoordinate.forEach((point) => {
+    if ((point.i === i, point.j === j)) {
+      return true;
+    }
+  });
+  return false;
+};
+  const basin = (map, i, j) => {
+    if (map[i][j] === 1) return 0;
+    
+    map[i][j] = 1;
+    let size = 1;
+    if (i - 1 > -1) {
+      size += basin(map, i - 1, j);
+    } 
+    if (i + 1 < map.length) {
+      size += basin(map, i + 1, j);
+    } 
+    if (j - 1 > -1) {
+      size += basin(map, i, j - 1);
+    } 
+    if (j + 1 < map[i].length) {
+      size += basin(map, i, j + 1);
+    }
+    return size;
+  };
+  
+
+function main(heightMap) {
+  const map = Array(heightMap.length)
+    .fill(Array(heightMap[0].length).fill(0))
+    .map((x, i) => x.map((x, j) => (heightMap[i][j] === 9 ? 1 : 0)));
+  const basins = [];
+
+  for (let i = 0; i < heightMap.length; i++) {
+    for (let j = 0; j < heightMap[i].length; j++) {
+      let basinQuantity = basin(map, i, j)
+      if (basinQuantity !== 0) {        
+        basins.push(basinQuantity);
       }
     }
   }
-  return arr.reduce((acc, curr) => acc + curr);
+  basins.sort((a, b) => b - a)
+  return basins[0] * basins[1] * basins[2]
 }
 
 console.log(main(parsingData(data)));
