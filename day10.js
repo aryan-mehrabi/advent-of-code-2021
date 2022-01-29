69db4b021bdf5d8d666bfd36a8fa45ba135f0dc3
aryan <aryan.me77@yahoo.com>
2022-01-29 17:35:05 +0330

    day10 part2

--- day10.js
+++ day10.js
@@ -13,7 +13,7 @@ const parsingData = (data) => {
   return data.split("\n").map((e) => e.trim());
 };
 
- const OPENING_BRACKETS = ["(", "[", "{", "<"].map(e => e.charCodeAt(0));
+const OPENING_BRACKETS = ["(", "[", "{", "<"].map(e => e.charCodeAt(0));
 const CLOSING_BRACKETS = [")", "]", "}", ">"].map((e) => e.charCodeAt(0));
 
 const isClosingBracket = (characterCode) => {
@@ -32,18 +32,33 @@ const incorrectCharecter = (line) => {
       openBrackets.push(character);
     }
   }
+  // part 2
+  return closingBracketsScore(openBrackets);
 };
+
+const closingBracketsScore = (openBrackets) => {
+  return openBrackets.reduceRight((closingBrac, openingBrac) => {
+    const indexOpening = OPENING_BRACKETS.indexOf(openingBrac);
+    return closingBrac.concat([indexOpening + 1]);
+  }, [])
+}
 
 const totalSyntaxError = (input) => {
   const errorPoints = [3, 57, 1197, 25137];
   let errors = [];
+  let missingBracketsScore = [];
   for (let i = 0; i < input.length; i++) {
     const line = input[i];
     const missingCharecter = incorrectCharecter(line);
-    if (missingCharecter) {
+    
+    if (typeof missingCharecter === "number") {
       errors.push(errorPoints[CLOSING_BRACKETS.indexOf(missingCharecter)]);
+    } 
+    else {
+      missingBracketsScore.push(missingCharecter.reduce((acc, current) => (acc * 5) + current))
     }
   }
-  return errors.reduce((acc, value) => acc + value);
+  return missingBracketsScore.sort((a,b) => a - b)[Math.floor(missingBracketsScore.length / 2)];
 };
 console.log(totalSyntaxError(parsingData(input)));
+
