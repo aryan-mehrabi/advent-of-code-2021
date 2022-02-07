c97381509aa546a7d73a8e27e2966e25afef839e
aryan <aryan.me77@yahoo.com>
2022-02-08 00:25:55 +0330

    day14 part2

--- day14.js
+++ day14.js
@@ -1,84 +1,63 @@
-const input = `NNCB
-
-CH -> B
-HH -> N
-CB -> H
-NH -> C
-HB -> C
-HC -> B
-HN -> C
-NN -> C
-BH -> H
-NC -> B
-NB -> B
-BN -> B
-BB -> N
-BC -> B
-CC -> N
-CN -> C`;
+const fs = require("fs")
+const input = fs
+.readFileSync("day14.txt", { encoding: "utf-8" }) // read day??.txt content
+.replace(/\r/g, "");
 
 const parsingData = (data) => {
   const [polymerTemplate, pairInsertions] = data.split("\n\n");
-  const updatedInsertion = pairInsertions.split("\n").reduce((object, pairInsertion) => {
-    const [pair, between] = pairInsertion.split(" -> ");
-    return Object.assign({}, {...object}, {[pair]: between})
-  }, {});
+  const updatedInsertion = pairInsertions
+    .split("\n")
+    .reduce((object, pairInsertion) => {
+      const [pair, between] = pairInsertion.split(" -> ");
+      return Object.assign(
+        {},
+        { ...object },
+        { [pair]: [pair[0] + between, between + pair[1]] }
+      );
+    }, {});
   return [polymerTemplate, updatedInsertion];
 };
 
-// [
-//     'NNCB',
-//     {
-//       CH: 'B',
-//       HH: 'N',
-//       CB: 'H',
-//       NH: 'C',
-//       HB: 'C',
-//       HC: 'B',
-//       HN: 'C',
-//       NN: 'C',
-//       BH: 'H',
-//       NC: 'B',
-//       NB: 'B',
-//       BN: 'B',
-//       BB: 'N',
-//       BC: 'B',
-//       CC: 'N',
-//       CN: 'C'
-//     }
-//   ]
-
 const polymerization = ([polymerTemplate, pairInsertion], steps) => {
-    const elementsOcurrance = {};
-    let i = 0;
+  const pairOcurrance = {};
+  let i = 0;
+  let pairs = {};
 
-    const insertion = (polymerTemplate) => {
-        let newPolymerTemplate = "";
-        if (i === 0) {
-            for (let index = 0; index < polymerTemplate.length; index++) {
-                const element = polymerTemplate[index];
-                elementsOcurrance[element] ? elementsOcurrance[element]++ : elementsOcurrance[element] = 1
-            }
-        }
+  for (let index = 0; index < polymerTemplate.length - 1; index++) {
+    pairs[polymerTemplate[index] + polymerTemplate[index + 1]] = (pairs[polymerTemplate[index] + polymerTemplate[index + 1]] || 0) + 1;
+  }
 
-        for (let index = 0; index < polymerTemplate.length - 1; index++) {
-            const pair = polymerTemplate[index] + polymerTemplate[index + 1];
-            const element = pairInsertion[pair]
-            elementsOcurrance[element] ? elementsOcurrance[element]++ : elementsOcurrance[element] = 1
-            newPolymerTemplate = newPolymerTemplate.concat(polymerTemplate[index], element)
-            if (index === polymerTemplate.length - 2) newPolymerTemplate += polymerTemplate[index + 1]
-        }
-        return newPolymerTemplate;
+  const lastCharacter = polymerTemplate[polymerTemplate.length - 1];
+
+  const insertion = (pairs) => {
+    const currentPairs = {};
+    const keys = Object.keys(pairs);
+
+    keys.forEach((key) => {
+      currentPairs[pairInsertion[key][0]] =
+        (currentPairs[pairInsertion[key][0]] || 0) + pairs[key];
+      currentPairs[pairInsertion[key][1]] =
+        (currentPairs[pairInsertion[key][1]] || 0) + pairs[key];
+    });
+    return currentPairs;
+  };
+
+  while (i < steps) {
+    pairs = insertion(pairs);
+    i++;
+  }
+  const eachElement = {};
+  eachElement[lastCharacter] = 1
+  for (const key in pairs) {
+    if (Object.hasOwnProperty.call(pairs, key)) {
+      const value = pairs[key];
+      eachElement[key[0]] = (eachElement[key[0]] || 0) + value
     }
+  }
+  const arr = Object.values(eachElement);
+  const min = Math.min(...arr)
+  const max = Math.max(...arr)
+  return max - min
+};
 
-    while (i < steps) {
-        polymerTemplate = insertion(polymerTemplate)
-        i++
-    }
-    
-    const arr = Object.values(elementsOcurrance).sort((a, b) => a - b)
-
-    return -arr[0] + arr[arr.length - 1]
-}
-
-console.log(polymerization(parsingData(input), 10))
+ console.log(polymerization(parsingData(input), 40));
